f7c378c38b651863fb6094a5d3cdd0f91b279c15
Abhishek Singh <abhishek.singh3@fiserv.com>
2022-05-03 13:41:16 -0400

    Removing

--- validator/download-api-zip.js
+++ validator/download-api-zip.js
@@ -39,7 +39,7 @@ const generateZipCollection = async (dir) => {
                zip.addFile(`${folder}/${file.name}`, Buffer.from(content, "utf8"), "entry comment goes here");  
             }  
              downloadFile = 'tennat_${apiVersion}_spec';   
-             await zip.writeZip(`/home/runner/work/Developer-Portal-Tenant-API/Developer-Portal-Tenant-API/${downloadFile}.zip`); 
+             await zip.writeZip(`${downloadFile}.zip`); 
              console.log(`File downloaded ---${file.name}`); 
           } 
         } catch (e) {
